96074ac294cf667798f14a0c6c031ebf7ecb4057
Yaroslav Getman <yaroslav.getman@aalto.fi>
2024-09-16 14:52:28 +0300

    Create prepare_fairseq_manifest.sh

---
+++ scripts/data_preprocessing/prepare_fairseq_manifest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=prepare_fairseq_manifest
+#SBATCH --output=/prepare_fairseq_manifest_radio_station_1.o
+#SBATCH --error=/prepare_fairseq_manifest_radio_station_1.e
+#SBATCH --partition=small
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=0-12:00:00
+#SBATCH --account=project_462000187
+
+module load LUMI/23.09
+module load FFmpeg/6.0-cpeGNU-23.09
+start_time=$(date +%s.%N)
+channel=radio_station_1
+path_to_manifests=/fairseq_manifests_tar
+mkdir -p ${path_to_manifests}/${channel}
+source /my_python_env/bin/activate
+python /scripts/data_preprocessing/prepare_fairseq_manifest.py ${path_to_manifests} $channel
+end_time=$(date +%s.%N)
+elapsed_time_seconds=$(echo "$end_time - $start_time" | bc)
+elapsed_time_minutes=$(echo "scale=2; $elapsed_time_seconds / 60" | bc)
+echo "Elapsed time: $elapsed_time_minutes minutes"
